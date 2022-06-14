bcf6e9380edc27e7122aeb5af6da20e643dc7fed
KOU James <hjkou@qq.com>
2022-06-14 12:59:49 +0800

    Fixed the path for the firmware dir outside the container

--- scripts/cisteps/build-openwrt/14-organize_files.sh
+++ scripts/cisteps/build-openwrt/14-organize_files.sh
@@ -20,8 +20,8 @@ if [ "x${OPT_PACKAGE_ONLY}" != "x1" ]; then
     [ ${#all_firmware_files[@]} -gt 0 ] && mv "${all_firmware_files[@]}" "${HOST_WORK_DIR}/openwrt_firmware/" || true
   fi
 
-  if [ -d "${OPENWRT_IB_FIRMWARE_DIR}" ]; then
-    cd ${OPENWRT_IB_FIRMWARE_DIR}
+  if [ -d "${HOST_WORK_DIR}/${OPENWRT_IB_FIRMWARE_DIR}" ]; then
+    cd ${HOST_WORK_DIR}/${OPENWRT_IB_FIRMWARE_DIR}
     all_firmware_files=(*)
     "${HOST_WORK_DIR}/openwrt_firmware/"
     [ ${#all_firmware_files[@]} -gt 0 ] && mv -f "${all_firmware_files[@]}" "${HOST_WORK_DIR}/openwrt_firmware/" || true
