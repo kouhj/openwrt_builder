11d57b33feb670bcdc6f3b4a775526a0f5028762
KOU James <hjkou@qq.com>
2022-06-17 18:40:23 +0800

    Added upload artifacts to transfer.sh

---
+++ scripts/cisteps/build-openwrt/15_upload_to_transfer.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#========================================================================================
+# https://github.com/kouhj/openwrt_builder
+# Description: Automatic Build of Custom OpenWRT Images with Official ImageBuilder & SDK
+#      This file contains the script to upload built artifacts to transfer.sh
+# Lisence: MIT
+# Author: kouhj
+#========================================================================================
+
+set -eo pipefail
+
+[ "x${TEST}" != "x1" ] || exit 0
+
+# To load the ENV variables that were set inside the docker
+source "${HOST_WORK_DIR}/scripts/lib/builder.sh"
+
+transfer() {
+  curl --progress-bar --upload-file "$1" https://transfer.sh/$(basename "$1") | tee /dev/null
+  echo
+}
+
+if [ -d "${HOST_WORK_DIR}/${OPENWRT_IB_FIRMWARE_DIR}" ]; then
+  cd ${HOST_WORK_DIR}/${OPENWRT_IB_FIRMWARE_DIR}
+  all_firmware_files=(!(*kernel*|*rootfs*|*firmware*))
+  FW_ARTIFACTS_FN="OpenWrt_firmware_${BUILD_TARGET}_${FILE_DATE}.tar"
+  if [ ${#all_firmware_files[@]} -gt 0 ]; then
+    tar cf $FW_ARTIFACTS_FN "${all_firmware_files[@]}"
+    FW_ARTIFACTS_URL=$(transfer $FW_ARTIFACTS_FN)
+    echo "::set-output name=archive::$PWD/${FW_ARTIFACTS_FN}}"
+    echo "::set-output name=url::${FW_ARTIFACTS_URL}}"
+  fi
+fi
