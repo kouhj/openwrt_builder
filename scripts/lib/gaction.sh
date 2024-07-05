335e03ea4faba70558698fe2acdd219bba208953
kouhj <hjkou@msn.com>
2024-07-05 00:50:06 +0000

    Create the vars file if it does not exist

--- scripts/lib/gaction.sh
+++ scripts/lib/gaction.sh
@@ -29,6 +29,7 @@ __save_var_to_file() {
 		  sed -i -r "s~^.*($var_name)=.*\$~\1=\"$var_value\"~" "$var_file"
 	  else # this var does not exist in the file
       echo "setting ${var_name}=\"${var_value}\" to $var_file"
+      [ -f "$var_file" ] || touch "$var_file"
       echo "${var_name}=\"${var_value}\"" >> "$var_file"
 	  fi
 }
