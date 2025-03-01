a7ce0505f4fd4081c376d76a491defb521f81de5
kouhj <hjkou@msn.com>
2025-03-01 16:43:45 +0800

    Refactor fetch_chnroutes prepare rootfs hook script to use  fetch-chnroutes.sh directly

--- user/default/ib/prepare_rootfs_hook.d/250_fetch_chnroutes.sh
+++ user/default/ib/prepare_rootfs_hook.d/250_fetch_chnroutes.sh
@@ -1,10 +1,2 @@
-echo Fetching the latest China routes ... this may take a while ...
-curl 'http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest' 2>/dev/null |
-    grep CN > /tmp/chnroutes.full
-
-if [ $? -eq 0 ]; then
-    grep ipv4 /tmp/chnroutes.full | awk -F\| '{ printf("%s/%d\n", $4, 32-log($5)/log(2)) }' > $1/etc/chnroutes.ipv4
-    grep ipv6 /tmp/chnroutes.full | awk -F\| '{ printf("%s/%d\n", $4, $5) }' > $1/etc/chnroutes.ipv6
-fi
-
-rm -f /tmp/chnroutes.* 2>/dev/null
+export IPKG_INSTROOT=$1
+sh -x ${IPKG_INSTROOT}/usr/bin/fetch-chnroutes.sh
