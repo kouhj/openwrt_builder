5930d1271ee1b3851bf40a3cb6577bb7f110755b
kouhj <hjkou@msn.com>
2025-03-08 16:02:09 +0800

    again...

--- scripts/cisteps/build-openwrt/07-download_openwrt.sh
+++ scripts/cisteps/build-openwrt/07-download_openwrt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -xeo pipefail
+set -eo pipefail
 
 # shellcheck disable=SC1090
 source "${HOST_WORK_DIR}/scripts/host/docker.sh"
@@ -12,6 +12,7 @@ source "${HOST_WORK_DIR}/scripts/host/docker.sh"
 # $OPENWRT_SDK_DIR:   OpenWRT SDK extracted dir
 # $MY_DOWNLOAD_DIR:   Where the files are downloaded by update_ib_sdk.sh
 
+set -x
 docker_exec "${BUILDER_CONTAINER_ID}" "${BUILDER_WORK_DIR}/scripts/update_ib_sdk.sh"
 docker_exec "${BUILDER_CONTAINER_ID}" "${BUILDER_WORK_DIR}/scripts/update_repo.sh"
 docker_exec "${BUILDER_CONTAINER_ID}" "${BUILDER_WORK_DIR}/scripts/update_feeds.sh"
--- scripts/update_feeds.sh
+++ scripts/update_feeds.sh
@@ -22,8 +22,12 @@ set -x
 generate_source_feeds_conf
 
 cd "${OPENWRT_CUR_DIR}"
+echo "Updating feeds ..."
 ./scripts/feeds update -a
+echo "Exit status of feeds update: $?"
+echo "Installing feeds ..."
 ./scripts/feeds install -a
+echo "Exit status of feeds install: $?"
 
 PACKAGE_DEFAULT_ROOT="package/openwrt-packages"
 
