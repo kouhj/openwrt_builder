ad675f5947b59e4bf0906073a3bfa9f0f8ba6e8e
kouhj <hjkou@msn.com>
2025-03-19 23:15:14 +0800

    Source builder.sh for download_dl_cache.sh

--- scripts/download_dl_cache.sh
+++ scripts/download_dl_cache.sh
@@ -4,6 +4,7 @@
 # From https://github.com/P3TERX/Actions-OpenWrt
 
 set -xeo pipefail
+source ${BUILDER_WORK_DIR}/scripts/lib/builder.sh
 
 if [ -z "${OPENWRT_COMPILE_DIR}" ] || [ -z "${OPENWRT_CUR_DIR}" ] || [ -z "${OPENWRT_SOURCE_DIR}" ]; then
   echo "::error::'OPENWRT_COMPILE_DIR', 'OPENWRT_CUR_DIR' or 'OPENWRT_SOURCE_DIR' is empty" >&2
@@ -15,9 +16,9 @@ fi
 DL_CACHE_DIR="${BUILDER_WORK_DIR}/dl_cache"
 
 cd "${OPENWRT_SDK_DIR}"
-source <( grep DOWNLOAD .config )
 make download -j8
 rm -rf ${DL_CACHE_DIR}/*
+source <( grep DOWNLOAD .config ) # Load CONFIG_DOWNLOAD_FOLDER from .config
 cp -a ${CONFIG_DOWNLOAD_FOLDER}/*  ${DL_CACHE_DIR}/
 
 # Cache the feature library
