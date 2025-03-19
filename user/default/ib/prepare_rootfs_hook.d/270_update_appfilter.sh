82a94eb151ab72572afe66513533f0908616bc56
kouhj <hjkou@msn.com>
2025-03-20 00:58:56 +0800

    tar forgot -C

--- user/default/ib/prepare_rootfs_hook.d/270_update_appfilter.sh
+++ user/default/ib/prepare_rootfs_hook.d/270_update_appfilter.sh
@@ -1,7 +1,7 @@
 export IPKG_INSTROOT=$1
 
 FEATURE_LIB="${DL_CACHE_DIR}/feature_lib.tar.gz"
-tar zxf $FEATURE_LIB ./feature.cfg ${IPKG_INSTROOT}/etc/appfilter/
-tar zxf $FEATURE_LIB ./app_icons ${IPKG_INSTROOT}/www/luci-static/resources/
+tar zxf $FEATURE_LIB ./feature.cfg -C ${IPKG_INSTROOT}/etc/appfilter/
+tar zxf $FEATURE_LIB ./app_icons -C ${IPKG_INSTROOT}/www/luci-static/resources/
 
 exit 0
